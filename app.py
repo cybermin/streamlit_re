08e36d72f649a91750c0168126e34650385ac45b
cybermin <sillamin@naver.com>
2022-08-22 19:50:07 +0900

    app

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import numpy as np
 data = pd.read_csv('기숙사수용현황분석.csv')
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1.metric("대학수", "70 °F", "1.2 °F")
+col2.metric("국공립학교", "9 mph", "-8%")
+col3.metric("사립학교", "86%", "4%")
 
 #st.dataframe(data)
 
